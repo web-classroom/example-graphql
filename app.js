bde581229ab1f64bcf2f580da5b34112a1378a4d
Bertil Chapuis <bchapuis@gmail.com>
2021-01-05 12:49:40 +0100

    Add schema

--- app.js
+++ app.js
@@ -1,18 +1,60 @@
 var express = require('express');
-var graphqlHTTP = require('express-graphql');
+var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
-// Construct a schema, using GraphQL schema language
+
+// Construct a schema, using the GraphQL schema language
 var schema = buildSchema(`
+  type Student {
+    firstname: String!
+    lastname: String!
+    age: Int!
+  }
+
+  type Classroom {
+    name: String!
+    students: [Student!]!
+  }
+
   type Query {
-    hello: String
+    classroom(name: String): Classroom
   }
 `);
 
+// Query
+/*
+{
+  classroom(name: "test") {
+    name,
+    students {
+      firstname
+    }
+  }
+}
+*/
+
+class Classroom {
+  constructor(name, students) {
+    this.name = name;
+    this.students = students;
+  }
+}
+
+class Student {
+  constructor(firstname, lastname, birthyear) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.birthyear = birthyear;
+  }
+  age() {
+    return new Date().getFullYear() - this.birthyear;
+  }
+}
+
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
+  classroom: ({name}) => {
+    return new Classroom(name, [new Student("George", "Edouard", 1990), new Student("Charles", "Gustave", 1992)]);
   },
 };
 
